d7339d59d58e63af6584cd727dcff39a88d8bcc4
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2016-12-19 13:41:03 +0100

    Fix ordering issue between dnsmasq and resolv_conf
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/roles/manifests/gateway.pp
+++ modules/roles/manifests/gateway.pp
@@ -6,6 +6,6 @@ class roles::gateway {
   include profile_lxc
   include ::resolv_conf
 
-  Class['profile_router']->Class['profile_dnsmasq']->Class['profile_lxc']
+  Class['profile_router']->Class['profile_dnsmasq']->Class['resolv_conf']->Class['profile_lxc']
 
 }
