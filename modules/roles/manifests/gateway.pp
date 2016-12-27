e371ab7c42a7a710d0a427c46aef37ef1b5591de
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2016-12-27 14:29:16 +0100

    Add squid module and profile_squid
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/roles/manifests/gateway.pp
+++ modules/roles/manifests/gateway.pp
@@ -4,6 +4,7 @@ class roles::gateway {
   include profile_router
   include profile_dnsmasq
   include profile_lxc
+  include profile_squid
   include ::resolv_conf
 
   Class['profile_router']->Class['profile_dnsmasq']->Class['resolv_conf']->Class['profile_lxc']
