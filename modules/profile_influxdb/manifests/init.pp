8a4dadf83861bf164521709c08f0879087618368
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-03 00:29:33 +0100

    Move more options to profile_influxdb and set firewall rules
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_influxdb/manifests/init.pp
+++ modules/profile_influxdb/manifests/init.pp
@@ -1,5 +1,58 @@
-class profile_influxdb {
+class profile_influxdb (
+  $http_log_enabled = false,
+  $collectd_port = '25826',
+  $restrict_to_interface = '',
+) {
 
-  include ::influxdb::server
+  class {'::influxdb::server':
+    http_log_enabled => $http_log_enabled,
+    collectd_options => {
+      'enabled'       => 'true',
+      'bind-address'  => ":${collectd_port}",
+    }
+  }
+
+  if empty($restrict_to_interface) {
+
+    firewall {'080 accept Influxdb collectd port':
+      proto   => 'udp',
+      dport   => $collectd_port,
+      action  => 'accept',
+    }
+
+    firewall {'080 accept Influxdb webinterface':
+      proto   => 'tcp',
+      dport   => '8083',
+      action  => 'accept',
+    }
+
+    firewall {'080 accept Influxdb api':
+      proto   => 'tcp',
+      dport   => '8086',
+      action  => 'accept',
+    }
+
+  } else {
+    firewall {"080 accept Influxdb collectd port from ${restrict_to_interface}":
+      proto   => 'udp',
+      dport   => $collectd_port,
+      iniface => $restrict_to_interface,
+      action  => 'accept',
+    }
+
+    firewall {"080 accept Influxdb webinterface from ${restrict_to_interface}":
+      proto   => 'tcp',
+      dport   => '8083',
+      iniface => $restrict_to_interface,
+      action  => 'accept',
+    }
+
+    firewall {"080 accept Influxdb api from ${restrict_to_interface}":
+      proto   => 'tcp',
+      dport   => '8086',
+      iniface => $restrict_to_interface,
+      action  => 'accept',
+    }
+  }
 
 }
