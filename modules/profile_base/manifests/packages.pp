90cd1e7a2cb88a188ad841c1063df2fbfed8358d
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-02 22:12:12 +0100

    Add less and vim-enhanced, remove vim-minimal from base packages
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_base/manifests/packages.pp
+++ modules/profile_base/manifests/packages.pp
@@ -3,9 +3,10 @@ class profile_base::packages {
   $default_package_list = [
     'htop',
     'nano',
-    'vim-minimal',
+    'vim-enhanced',
     'tcpdump',
     'dhcping',
+    'less',
   ]
 
   package { $default_package_list:
