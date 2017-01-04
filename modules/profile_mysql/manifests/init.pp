835ee6c15079a2bce7145ae5f42f2c8f2a223639
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-04 11:02:47 +0100

    Extend profile_mysql and add monitoring user
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_mysql/manifests/init.pp
+++ modules/profile_mysql/manifests/init.pp
@@ -1,5 +1,29 @@
-class profile_mysql {
+class profile_mysql (
+  $root_password,
+  $monitor_password,
+  $monitor_hostname = '%',
+  $remove_default_accounts = true,
+  $override_options = {},
+  $databases = {},
+  $users = {},
+  $grants = {},
+) {
 
-  include ::mysql::server
+  class {'::mysql::server':
+    root_password           => $root_password,
+    remove_default_accounts => $remove_default_accounts,
+    override_options        => $override_options,
+    databases               => $databases,
+    users                   => $users,
+    grants                  => $grants,
+  }
+
+  class {'::mysql::server::monitor':
+    mysql_monitor_username => 'monitor',
+    mysql_monitor_password => $monitor_password,
+    mysql_monitor_hostname => $monitor_hostname,
+  }
+
+  
 
 }
