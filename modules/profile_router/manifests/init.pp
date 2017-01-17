902ea01713940efd762f0ad27e14c8a7deb34f6c
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-17 21:58:56 +0100

    Allow passing forwarding firewall rules
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_router/manifests/init.pp
+++ modules/profile_router/manifests/init.pp
@@ -2,19 +2,29 @@ class profile_router (
   $wan_interface = $::interfaces[0],
   $natted_lans = {},
   $bridges = {},
+  $restrict_forwarding = false,
+  $forwarding_rules = {},
 ) {
+
+  include ::openvswitch
 
   sysctl::value {"net.ipv4.ip_forward": value => "1"}
 
-  firewall{"888 forward anything":
-    proto  => 'all',
-    chain  => 'FORWARD',
-    action => 'accept',
+  if $restrict_forwarding {
+    firewall{'999 drop all':
+      proto  => 'all',
+      chain  => 'FORWARD',
+      action => 'drop',
+      before => undef,
+    }
   }
 
-  include ::openvswitch
-  create_resources('openvswitch::bridge', $bridges)
+  $forwarding_defaults = {
+    'chain' => 'FORWARD',
+  }
 
+  create_resources('firewall', $forwarding_rules, $forwarding_defaults)
+  create_resources('openvswitch::bridge', $bridges)
   create_resources('profile_router::natted_lan', $natted_lans)
 
 }
