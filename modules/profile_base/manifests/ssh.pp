b4364835988e355b42d08ad65d9d0cdde0df2afa
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-01-18 21:59:48 +0100

    Allow restricting ssh access from an interface
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_base/manifests/ssh.pp
+++ modules/profile_base/manifests/ssh.pp
@@ -1,4 +1,6 @@
-class profile_base::ssh {
+class profile_base::ssh (
+  $restrict_to_interface = undef,
+) {
 
   class { 'ssh::server':
     storeconfigs_enabled => false,
@@ -8,10 +10,19 @@ class profile_base::ssh {
     },
   }
 
-  firewall { '006 accept SSH':
-    dport    => [22,2222],
-    proto    => tcp,
-    action   => accept,
+  if $restrict_to_interface {
+    firewall { "006 accept SSH from ${restrict_to_interface}":
+      dport    => [22,2222],
+      proto    => tcp,
+      iniface  => $restrict_to_interface,
+      action   => accept,
+    }
+  } else {
+    firewall { '006 accept SSH':
+      dport    => [22,2222],
+      proto    => tcp,
+      action   => accept,
+    }
   }
 
 }
