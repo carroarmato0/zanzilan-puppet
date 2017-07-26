344ce569af1a3dc1f53f1452407b5b2c6221560a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-07-26 20:35:28 +0200

    Manage pip an and related python stuff ourselves
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_graphite/manifests/init.pp
+++ modules/profile_graphite/manifests/init.pp
@@ -4,12 +4,21 @@ class profile_graphite (
 ) {
 
   class {'::graphite':
-    gr_timezone     => $timezone,
-    secret_key      => $secret_key,
-    gr_pip_install  => true,
+    gr_timezone               => $timezone,
+    secret_key                => $secret_key,
+    gr_pip_install            => true,
+    gr_manage_python_packages => false,
   }
 
   package { 'python2-pip':
+    ensure => installed,
+  }
+
+  package { 'python-devel':
+    ensure => installed,
+  }
+
+  package { 'gcc':
     ensure => installed,
   }
 
