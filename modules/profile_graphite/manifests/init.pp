28d61f9a44fe7ac1996f055a2ab082005ed9c32a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-07-26 20:24:39 +0200

    Specify version of graphite
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_graphite/manifests/init.pp
+++ modules/profile_graphite/manifests/init.pp
@@ -1,12 +1,14 @@
 class profile_graphite (
   $secret_key,
-  $timezone   = 'Europe/Brussels',
+  $timezone         = 'Europe/Brussels',
+  $graphite_version = '0.9.15-1.el7',
 ) {
 
   class {'::graphite':
-    gr_timezone    => $timezone,
-    secret_key     => $secret_key,
-    gr_pip_install => false,
+    gr_timezone      => $timezone,
+    secret_key       => $secret_key,
+    gr_pip_install   => false,
+    graphite_version => $graphite_version,
   }
 
   package { 'python2-pip':
