bfa876004abad644f342514b05090ae2e75500b6
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-07-26 20:31:18 +0200

    Install graphite through pip to avoid package hell
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/profile_graphite/manifests/init.pp
+++ modules/profile_graphite/manifests/init.pp
@@ -1,14 +1,12 @@
 class profile_graphite (
   $secret_key,
   $timezone         = 'Europe/Brussels',
-  $graphite_version = '0.9.15-1.el7',
 ) {
 
   class {'::graphite':
     gr_timezone     => $timezone,
     secret_key      => $secret_key,
-    gr_pip_install  => false,
-    gr_graphite_ver => $graphite_version,
+    gr_pip_install  => true,
   }
 
   package { 'python2-pip':
