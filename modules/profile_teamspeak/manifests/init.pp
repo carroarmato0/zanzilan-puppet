7c728dae9bc2887003c51882092855537f4243ef
Christophe Vanlancker <carroarmato0@gmail.com>
2017-08-01 10:55:11 +0200

    Add fw rules for teamspeak
    Signed-off-by: Christophe Vanlancker <carroarmato0@gmail.com>

--- modules/profile_teamspeak/manifests/init.pp
+++ modules/profile_teamspeak/manifests/init.pp
@@ -1,13 +1,38 @@
 class profile_teamspeak (
-    $version      = '3.0.13.8',
-    $license_file = '',
+    $version             = '3.0.13.8',
+    $license_file        = '',
+    $allow_filetransfers = false,
 ) {
+
+  $accept_filetransfers = $allow_filetransfers ? {
+    true    => 'accept',
+    false   => 'reject',
+    default => 'reject',
+  }
 
   class {'::teamspeak':
     version      => $version,
     arch         => 'amd64',
     license_file => $license_file,
     init         => 'systemd',
+  }
+
+  firewall {'080 accept Teamspeak Voice':
+    proto   => 'udp',
+    dport   => '9987',
+    action  => 'accept',
+  }
+
+  firewall {'080 accept Teamspeak Server Query':
+    proto   => 'tcp',
+    dport   => '10011',
+    action  => 'accept',
+  }
+
+  firewall {'080 accept Teamspeak Filetransfer':
+    proto   => 'tcp',
+    dport   => '30033',
+    action  => $accept_filetransfers,
   }
 
 }
