742804a7e299cca748fac0e26df5b4d5c796af79
Christophe Vanlancker <carroarmato0@inuits.eu>
2018-07-12 22:31:16 +0200

    Allow setting the dns lookup for the service role
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- modules/roles/manifests/service.pp
+++ modules/roles/manifests/service.pp
@@ -3,7 +3,9 @@ class roles::service {
   include profile_base
   include profile_router
   include profile_lxc
-  #include profile_squid
-  #include profile_grafana
+  include ::resolv_conf
+
+  Class['profile_router']->
+  Class['resolv_conf']
 
 }
